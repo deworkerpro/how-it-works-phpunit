0e91314fce30b6b264ce04ec7d9bd3a4666e8c47
ElisDN <mail@elisdn.ru>
2023-11-03 13:50:27 +0300

    Add exit code

--- bin/test.php
+++ bin/test.php
@@ -8,4 +8,7 @@ require_once __DIR__ . '/../src/normalizeEmail.php';
 
 if ('mail@app.test' !== normalizeEmail('mail+suffix@app.test')) {
     echo 'Values are not equal' . PHP_EOL;
+    exit(1);
 }
+
+exit(0);
