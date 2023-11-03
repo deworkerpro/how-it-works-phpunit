5dc512e375271ff9bc7b64228d4bcb9f845d1381
ElisDN <mail@elisdn.ru>
2023-11-03 14:20:39 +0300

    Remove side effects

--- bin/test.php
+++ bin/test.php
@@ -6,20 +6,26 @@ use function App\normalizeEmail;
 
 require_once __DIR__ . '/../src/normalizeEmail.php';
 
-function assertEquals(string $expected, string $actual, bool &$success): void
+final class AssertException extends LogicException {}
+
+function assertEquals(string $expected, string $actual): void
 {
     if ($actual !== $expected) {
-        $success = false;
-        echo sprintf('Actual value "%s" is not equal to "%s"', $actual, $expected) . PHP_EOL;
+        throw new AssertException(sprintf('Actual value "%s" is not equal to "%s"', $actual, $expected));
     }
 }
 
 $success = true;
 
-assertEquals('mail@app.test', normalizeEmail('mail@app.test'), $success);
-assertEquals('mail@app.test', normalizeEmail('mail+suffix@app.test'), $success);
-assertEquals('mail@app.test', normalizeEmail('mail+dashed-suffix@app.test'), $success);
-assertEquals('mail@app.test', normalizeEmail('mail+double+suffix@app.test'), $success);
-assertEquals('dashed-mail@app.test', normalizeEmail('dashed-mail+suffix@app.test'), $success);
+try {
+    assertEquals('mail@app.test', normalizeEmail('mail@app.test'));
+    assertEquals('mail@app.test', normalizeEmail('mail+suffix@app.test'));
+    assertEquals('mail@app.test', normalizeEmail('mail+dashed-suffix@app.test'));
+    assertEquals('mail@app.test', normalizeEmail('mail+double+suffix@app.test'));
+    assertEquals('dashed-mail@app.test', normalizeEmail('dashed-mail+suffix@app.test'));
+} catch (AssertException $exception) {
+    $success = false;
+    echo 'FAIL: ' . $exception->getMessage() . PHP_EOL;
+}
 
 exit($success ? 0 : 1);
