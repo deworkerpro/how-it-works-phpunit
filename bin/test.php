8d82e26c835eb1e668f8082fd83f1b89b44d2ae7
ElisDN <mail@elisdn.ru>
2023-11-03 13:56:30 +0300

    Multiple tests

--- bin/test.php
+++ bin/test.php
@@ -8,7 +8,27 @@ require_once __DIR__ . '/../src/normalizeEmail.php';
 
 $success = true;
 
+if ('mail@app.test' !== normalizeEmail('mail@app.test')) {
+    $success = false;
+    echo 'Values are not equal' . PHP_EOL;
+}
+
 if ('mail@app.test' !== normalizeEmail('mail+suffix@app.test')) {
+    $success = false;
+    echo 'Values are not equal' . PHP_EOL;
+}
+
+if ('mail@app.test' !== normalizeEmail('mail+dashed-suffix@app.test')) {
+    $success = false;
+    echo 'Values are not equal' . PHP_EOL;
+}
+
+if ('mail@app.test' !== normalizeEmail('mail+double+suffix@app.test')) {
+    $success = false;
+    echo 'Values are not equal' . PHP_EOL;
+}
+
+if ('dashed-mail@app.test' !== normalizeEmail('dashed-mail+suffix@app.test')) {
     $success = false;
     echo 'Values are not equal' . PHP_EOL;
 }
