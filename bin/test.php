39922a0ddba346bb3f6a5333082e361f8c9e39ce
ElisDN <mail@elisdn.ru>
2023-11-03 14:08:12 +0300

    Add assert procedure

--- bin/test.php
+++ bin/test.php
@@ -6,46 +6,20 @@ use function App\normalizeEmail;
 
 require_once __DIR__ . '/../src/normalizeEmail.php';
 
+function assertEquals(string $expected, string $actual, bool &$success): void
+{
+    if ($actual !== $expected) {
+        $success = false;
+        echo sprintf('Actual value "%s" is not equal to "%s"', $actual, $expected) . PHP_EOL;
+    }
+}
+
 $success = true;
 
-$expected = 'mail@app.test';
-$actual = normalizeEmail('mail@app.test');
-
-if ($actual !== $expected) {
-    $success = false;
-    echo sprintf('Actual value "%s" is not equal to "%s"', $actual, $expected) . PHP_EOL;
-}
-
-$expected = 'mail@app.test';
-$actual = normalizeEmail('mail+suffix@app.test');
-
-if ($actual !== $expected) {
-    $success = false;
-    echo sprintf('Actual value "%s" is not equal to "%s"', $actual, $expected) . PHP_EOL;
-}
-
-$expected = 'mail@app.test';
-$actual = normalizeEmail('mail+dashed-suffix@app.test');
-
-if ($actual !== $expected) {
-    $success = false;
-    echo sprintf('Actual value "%s" is not equal to "%s"', $actual, $expected) . PHP_EOL;
-}
-
-$expected = 'mail@app.test';
-$actual = normalizeEmail('mail+double+suffix@app.test');
-
-if ($actual !== $expected) {
-    $success = false;
-    echo sprintf('Actual value "%s" is not equal to "%s"', $actual, $expected) . PHP_EOL;
-}
-
-$expected = 'dashed-mail@app.test';
-$actual = normalizeEmail('dashed-mail+suffix@app.test');
-
-if ($actual !== $expected) {
-    $success = false;
-    echo sprintf('Actual value "%s" is not equal to "%s"', $actual, $expected) . PHP_EOL;
-}
+assertEquals('mail@app.test', normalizeEmail('mail@app.test'), $success);
+assertEquals('mail@app.test', normalizeEmail('mail+suffix@app.test'), $success);
+assertEquals('mail@app.test', normalizeEmail('mail+dashed-suffix@app.test'), $success);
+assertEquals('mail@app.test', normalizeEmail('mail+double+suffix@app.test'), $success);
+assertEquals('dashed-mail@app.test', normalizeEmail('dashed-mail+suffix@app.test'), $success);
 
 exit($success ? 0 : 1);
