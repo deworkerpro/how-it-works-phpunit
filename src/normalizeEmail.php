bc6b75c90b2fc369c5142ffa39ca8affc2dad810
ElisDN <mail@elisdn.ru>
2023-11-10 17:16:33 +0300

    Implement function

--- src/normalizeEmail.php
+++ src/normalizeEmail.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace App;
 
+use InvalidArgumentException;
+
 function normalizeEmail(string $email): string
 {
-    return $email;
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        throw new InvalidArgumentException('Incorrect Email format.');
+    }
+
+    return preg_replace('/\+[^@]+@/', '@', $email);
 }
